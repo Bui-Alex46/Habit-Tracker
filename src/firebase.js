0dc5111afb429535a52e06c3a47a517d47673c16
Alex Bui <alex46bui@gmail.com>
2023-06-02 13:50:06 -0700

    Added local environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "@firebase/firestore";
 import {getAuth} from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyC_v4mDl86WK7CzyfMIpSXQo7dMWKT1dmg",
-  authDomain: "habittracker-3d266.firebaseapp.com",
-  projectId: "habittracker-3d266",
-  storageBucket: "habittracker-3d266.appspot.com",
-  messagingSenderId: "684276298452",
-  appId: "1:684276298452:web:bbc2947a9af25036b78141"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
